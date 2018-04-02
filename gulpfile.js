d6822a96d968e0aecf8fc5dff858ee7b6b4ca8b9
majorazero <daniel.cw.hsu@gmail.com>
2018-04-02 14:16:04 -0700

    trying to add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var gulp = require("gulp");
+var connect = require("gulp-connect"); //Runs a local dev server
+var open = require("gulp-open"); //Open a URL in a web browser
+
+var  config = {
+  port:9500,
+  devBaseUrl: "http://localhost",
+  paths: {
+    html: "./src/*.html", //go int the source directory and find anything that ends in .html
+    dist: "/dist"
+  }
+}
+
+//Start a local development server
+gulp.task("connect",function(){
+  connect.server({
+    root: ["dist"], //where the files are served from
+    port: config.port,
+    base: config.devBaseUrl,
+    livereload: true //self explanatory
+  });
+});
+
+gulp.task("open",["connect"],function(){ //runs connect first
+  gulp.src("/dist/index.html")
+    .pipe(open({uri: config.devBaseUrl + ":" + config.port +"/"}));
+});
+
+gulp.task("html",function(){
+  gulp.src(config.paths.html)
+    .pipe(gulp.dest(config.paths.dist))
+    .pipe(connect.reload()); //using npm installed dependencies
+});
+
+gulp.task("default",["html","open"]); //if you type gulp in command line it will run html and open
