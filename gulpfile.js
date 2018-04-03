7ff09b3f267ecc1cd8a905d0c646b9cc35249c06
majorazero <daniel.cw.hsu@gmail.com>
2018-04-02 17:10:18 -0700

    added really jenky circumvent of routing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 "use strict";
-
 var gulp = require("gulp");
 var connect = require("gulp-connect"); //Runs a local dev server
 var open = require("gulp-open"); //Open a URL in a web browser
@@ -23,7 +22,6 @@ var  config = {
     mainJs: "./src/main.js"
   }
 }
-
 //Start a local development server
 gulp.task("connect",function(){
   connect.server({
@@ -33,18 +31,15 @@ gulp.task("connect",function(){
     livereload: true //self explanatory
   });
 });
-
 gulp.task("open",["connect"],function(){ //runs connect first
   gulp.src("dist/index.html")
     .pipe(open({uri: config.devBaseUrl + ":" + config.port +"/"}));
 });
-
 gulp.task("html",function(){
   gulp.src(config.paths.html)
     .pipe(gulp.dest(config.paths.dist))
     .pipe(connect.reload()); //using npm installed dependencies
 });
-
 gulp.task("js",function(){
   browserify(config.paths.mainJs)
     .transform(reactify)
@@ -54,22 +49,18 @@ gulp.task("js",function(){
     .pipe(gulp.dest(config.paths.dist + "/scripts"))
     .pipe(connect.reload());
 });
-
 gulp.task("css",function(){
   gulp.src(config.paths.css)
     .pipe(concat("bundle.css"))
     .pipe(gulp.dest(config.paths.dist+"/css"));
 });
-
 gulp.task("lint",function(){
   return gulp.src(config.paths.js)
     .pipe(lint({config: "eslint.config.json"}))
     .pipe(lint.format());
 });
-
 gulp.task("watch",function(){
   gulp.watch(config.paths.html,["html"]); //if anything changes in paths it will reload the browser.
   gulp.watch(config.paths.js,["js","lint"]);
 });
-
 gulp.task("default",["html","js","css","lint","open","watch"]); //if you type gulp in command line it will run html and open
---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require("react");
+
+var Home = React.createClass({ //Object style of calling components?
+  render: function(){
+    return(
+      <div className="jumbotron">
+        <h1>Administration</h1>
+        <p>Reacting to some Routing and Fluxxing</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;
